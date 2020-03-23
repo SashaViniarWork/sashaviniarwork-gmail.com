450153b2a644d68e9a5a0d1bb036d4e5f215ed45
KaMeLoTmArMoT <aashebeko@ukr.net>
2020-03-23 15:10:44 +0200

    test login-service integrated.

--- Evort/src/app/app.component.ts
+++ Evort/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ApiLoginService} from './services/api-login.service';
 
 /**
  * header and footer for all pages
@@ -7,8 +8,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ApiLoginService]
 })
 export class AppComponent {
+  title = 'Evort';
+  movies = [{title: 'test'}, {title: 'test-2'}];
+  password = [{value: 123}];
 
+  constructor(private api: ApiLoginService) {
+    this.getMovies();
+    this.checkPassword();
+  }
+
+  getMovies = () => {
+    this.api.getAllMovies().subscribe(
+      data => {
+        this.movies = data;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+  checkPassword = () => {
+    this.api.checkPassword().subscribe(
+      data => {
+        this.password = data;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
--- Evort/src/app/app.module.ts
+++ Evort/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './components/registration/registration.component';
@@ -11,7 +12,7 @@ const appRoutes: Routes = [
   {path: '', component: WelcomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'registration', component: RegistrationComponent}
-]
+];
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
