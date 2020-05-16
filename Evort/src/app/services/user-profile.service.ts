2c168229448d214f52c2ae838891fb19f65cd039
Viktoria Zaremba <43505074+ViktoriaZaremba@users.noreply.github.com>
2020-05-17 00:35:42 +0300

    created user-profile component, service and static view

---
+++ Evort/src/app/services/user-profile.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { catchError, map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+/*
+export interface users {
+  id : bigint;
+  username : string;
+  firstName : string;
+  lastName : string;
+  email : string;
+  phone : string;
+  country : string;
+  address : string;
+  dateOfBirth : string;
+}
+*/
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserProfileService {
+
+  baseurl = 'https://evort.herokuapp.com';
+  httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
+
+  constructor(private http: HttpClient) { }
+
+  getAllUsersData(): Observable<any> {
+    return this.http.get(this.baseurl + '/users/');
+  }
+}
